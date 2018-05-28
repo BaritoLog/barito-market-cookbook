28af2066cd4cc3a12c59f5e9b95df4d6203f982f
Giovanni Sakti <giosakti@gmail.com>
2018-05-28 15:06:31 +0700

    [gio/tara] remove metadata.json because it causes conflict

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe BaritoMarketCookbook::default
+# Inspec test for recipe barito_market::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
