3606164acbe7256ffd0a5805e0aceaaf670f7edc
opan <opan.neutron@gmail.com>
2018-08-02 16:04:09 +0700

    [opan/tara] Add sidekiq service

--- recipes/sidekiq.rb
+++ recipes/sidekiq.rb
@@ -1,7 +1,17 @@
 apt_update
 package 'redis-server'
 
-service "redis" do
+template "/etc/systemd/system/sidekiq.service" do
+  source "sidekiq.service.erb"
+  owner user
+  group group
+  mode '0755'
+  variables app_directory: "#{install_directory}/BaritoMarket"
+  notifies :run, "execute[systemctl-daemon-reload]", :immediately
+  notifies :restart, "service[sidekiq]", :delayed
+end
+
+service "sidekiq" do
   action [:enable, :start]
   supports :status => true, :start => true, :restart => true, :stop => true
   provider Chef::Provider::Service::Systemd
