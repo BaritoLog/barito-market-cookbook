694dd021dde5e07590d612ebf8308e0bdd6937cd
opan <opan.neutron@gmail.com>
2018-08-02 14:27:38 +0700

    [opan/tara] Refactoring recipe puma

--- recipes/puma.rb
+++ recipes/puma.rb
@@ -6,29 +6,14 @@ env = node[cookbook_name]['env']
 
 gem_package 'puma'
 
-directory "#{install_directory}/shared/config" do
-  owner user
-  group group
-  mode '0755'
-  recursive true
-  action :create
-end
-
-directory "#{install_directory}/shared/tmp/state" do
-  owner user
-  group group
-  mode '0755'
-  recursive true
-  action :create
-end
-
-directory "#{install_directory}/shared/tmp/pids" do
-  owner user
-  group group
-  mode '0755'
-  recursive true
-  recursive true
-  action :create
+["#{install_directory}/shared/config", "#{install_directory}/shared/tmp/state", "#{install_directory}/shared/tmp/pids"].each do |path|
+  directory path do
+    owner user
+    group group
+    mode '0755'
+    recursive true
+    action :create
+  end
 end
 
 template "#{install_directory}/shared/config/puma.#{env}.rb" do
@@ -36,28 +21,9 @@ template "#{install_directory}/shared/config/puma.#{env}.rb" do
   owner user
   group group
   mode '0755'
-  recursive true
   variables directory: "#{install_directory}/BaritoMarket",
             environment: node[cookbook_name]['env'],
             pidfile_directory: "#{install_directory}/shared/tmp/pids",
             state_directory: "#{install_directory}/shared/tmp/state"
   mode "400"
 end
-
-# directory "/etc/puma" do
-  # owner 'root'
-  # group 'root'
-  # recursive true
-  # action :create
-# end
-
-# template "/etc/puma/#{app_name}.rb" do
-  # source "puma.conf.erb"
-  # owner user
-  # group group
-  # variables directory: "#{install_directory}/BaritoMarket",
-            # environment: node[cookbook_name]['env'],
-            # pidfile_path: "#{cookbook_name}/BaritoMarket",
-            # state_path: "#{cookbook_name}/BaritoMarket"
-  # mode "400"
-# end
