a7bf021d2f5ecd8458cfa2eb580d7b519dc42f83
opan <opan.neutron@gmail.com>
2018-08-02 10:49:28 +0700

    [opan/tara] Add step recipe systemd

--- recipes/systemd.rb
+++ recipes/systemd.rb
@@ -1,20 +1,15 @@
-app_name = node['app_name']
-market_script_location = node['market_script_location']
-command_name = node['command_name']
-github_repo = 'BaritoLog/BaritoMarket'
-release_name = Github.release_name(github_repo)
-env = node[app_name]['environment_variables']
+app_name = cookbook_name
+release_name = node[cookbook_name]['release_name']
+user = node[cookbook_name]['user']
+group = node[cookbook_name]['group']
 
 template "/etc/systemd/system/puma.service" do
   source "systemd.erb"
-  owner app_name
-  group app_name
-  mode "0644"
-  variables(
-            app_name: app_name,
-            app_home: app_name,
-            market_script_location: market_script_location
-           )
+  owner user
+  group group
+  mode '0644'
+  variables app_name: app_name,
+            user: user,
   notifies :run, "execute[systemctl-daemon-reload]", :immediately
   notifies :restart, "service[puma]", :delayed
 end
