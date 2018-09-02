bb008ff25b108fee0ea67d8e2ff31b4f2f4aaf6b
Giovanni Sakti <giosakti@gmail.com>
2018-09-02 22:29:54 +0700

    use ubuntu 18.04 as platform for testing by default

--- recipes/install.rb
+++ recipes/install.rb
@@ -6,7 +6,7 @@ apt_update
 
 package %w[
   software-properties-common ruby2.5 ruby2.5-dev nodejs build-essential patch
-  ruby-dev zlib1g-dev liblzma-dev ruby-switch libffi-dev libcurl4-openssl-dev
+  ruby-dev zlib1g-dev liblzma-dev libffi-dev libcurl4-openssl-dev
 ]
 
 gem_package 'bundler'
