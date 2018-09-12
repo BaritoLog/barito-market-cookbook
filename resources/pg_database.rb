5c06517d8ca2e84dcfceccbe3fbba7b15ea720a0
Tara Baskara <tara.baskara@gmail.com>
2018-09-12 17:36:41 +0700

    [tara/gio] Changed locale from en_US to C

--- resources/pg_database.rb
+++ resources/pg_database.rb
@@ -9,7 +9,7 @@
 property :database,  String, name_property: true
 property :user,      String, default: 'postgres', required: true
 property :encoding,  String, default: 'UTF-8'
-property :locale,    String, default: 'en_US.UTF-8'
+property :locale,    String, default: 'C.UTF-8'
 
 action :create do
   createdb = 'createdb'
